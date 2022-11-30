43f177208ffa9cdf596795b6f7223a7ca7864e96
Arvind R K <52023592+arvindrk47@users.noreply.github.com>
2022-11-30 21:59:34 +0530

    Create check_vector.cpp

---
+++ gfg/vector/check_vector.cpp
@@ -0,0 +1,4 @@
+/*1. Write a C++ program to check whether numbers in a vector can be rearranged so that each number appears exactly once in a consecutive list of numbers. Return true otherwise false.
+Example:
+1 2 5 0 3 6 7
+Check consecutive numbers in the said vector! 0 */
