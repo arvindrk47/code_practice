9aeca2367144d71c751b5d2c72070624c0a5bb2d
Arvind R K <52023592+arvindrk47@users.noreply.github.com>
2022-12-07 20:51:37 +0530

    Create MAKEAP.cpp

---
+++ codechef/MAKEAP.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b;
+	    cin>>a>>b;
+	    float c = float(a);
+	    float d = (float)b;
+	    if((a+b)/2 == (c+d)/2){
+	        cout<<(a+b)/2<<endl;
+	    }else{
+	        cout<<"-1"<<endl;
+	    }
+	}
+	
+	return 0;
+}
