5b243fc10cba4f963e6a69350480d4a9dc92c748
Arvind R K <52023592+arvindrk47@users.noreply.github.com>
2022-12-07 20:14:05 +0530

    Create BULLBEAR.cpp

---
+++ codechef/BULLBEAR.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a<b){
+	        cout<<"PROFIT"<<endl;
+	    }else if(a>b){
+	        cout<<"LOSS"<<endl;
+	    }else{
+	        cout<<"NEUTRAL"<<endl;
+	    }
+	}
+	return 0;
+}
