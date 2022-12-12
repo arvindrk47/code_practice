ca8f98a72e8569bac40cb7052d9e44c140349f5a
Arvind R K <52023592+arvindrk47@users.noreply.github.com>
2022-12-12 22:35:34 +0530

    Create left_triangle_number.cpp

---
+++ pattern-programs/left_triangle_numbers/left_triangle_number.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int size =5;
+    for(int i=0; i<size; i++){
+        for(int j=0; j<=i; j++){
+            cout<<j+1;
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
