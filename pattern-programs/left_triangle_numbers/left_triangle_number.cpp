18121ed0b8e12ca3c0abdc8f3390d46bada6d661
Arvind R K <52023592+arvindrk47@users.noreply.github.com>
2023-01-28 10:39:33 +0530

    Added Alternative for left_triangle_number.cpp

--- pattern-programs/left_triangle_numbers/left_triangle_number.cpp
+++ pattern-programs/left_triangle_numbers/left_triangle_number.cpp
@@ -14,3 +14,29 @@ int main() {
 
     return 0;
 }
+
+/*
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+void star(int n){
+    for(int i=0; i<=5; i++){
+        
+        for(int j=1; j<=i; j++){
+            cout<<j;
+        }
+        
+        cout<<endl;
+    }
+}
+
+int main() {
+    // Write C++ code here
+    int a;
+    cin>>a;
+    star(a);
+    return 0;
+}
+*/
