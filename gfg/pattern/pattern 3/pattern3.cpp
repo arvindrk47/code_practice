39aa5df44636ae4325c9bd997082fcc0a0f701b4
Arvind R K <52023592+arvindrk47@users.noreply.github.com>
2023-01-29 21:20:17 +0530

    Added changes in pattern3.cpp

--- gfg/pattern/pattern 3/pattern3.cpp
+++ gfg/pattern/pattern 3/pattern3.cpp
@@ -8,7 +8,7 @@ class Solution {
   public:
     void printTriangle(int n) {
         // code here
-        for(int i=0; i<=n; i++){
+        for(int i=1; i<=n; i++){
             for(int j=1; j<=i;j++){
                 cout<<j<<" ";
             }
