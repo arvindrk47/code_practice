2e0b767bec19602a9a6387f15dc4851f51d943e2
Arvind R K <52023592+arvindrk47@users.noreply.github.com>
2023-02-08 22:05:42 +0530

    Create BTRYHLTH.cpp

---
+++ codechef/BTRYHLTH.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    if(a >= 80){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
